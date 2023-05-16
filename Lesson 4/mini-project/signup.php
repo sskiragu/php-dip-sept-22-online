21e782589321746caaa1611d726be6a7bb7b9eff
sskiragu <skiragu@gmail.com>
2023-05-16 18:56:01 +0300

    Signup user

--- Lesson 4/mini-project/signup.php
+++ Lesson 4/mini-project/signup.php
@@ -11,5 +11,42 @@
 <?php include_once "navbar.php"; ?>
 
 <!-- Signup form -->
+<div>
+    <h1>Signup Page</h1>
+    <form action="signup.php" method="post">
+        <div><input type="text" name="username" placeholder="Enter username"></div>
+        <div><input type="email" name="email" placeholder="Enter email"></div>
+        <div><input type="password" name="password" placeholder="Enter password"></div>
+        <div><input type="submit" name="signup" value="Signup"></div>
+    </form>
+</div>
 </body>
 </html>
+<?php
+if (isset($_POST['signup'])) {
+    
+    // Getting the form values
+    $username =  $_POST['username'];
+    $email =  $_POST['email'];
+    $password = $_POST['password'];
+
+    //Create a connection to the database
+   $database_connection = new mysqli("localhost", "root", "", "kcb");
+   if ($database_connection->connect_error) {
+    echo $database_connection->error;
+   }else{
+    echo "Database connection successfull.";
+   }
+
+    // Inser the values
+    $sql = "INSERT INTO users(username, email, password) VALUES('$username', '$email', '$password')";
+    $database_connection->query($sql);
+    echo "<br>";
+    if($database_connection->query($sql) === TRUE){
+        echo "Signup successfull";
+    }else{
+        echo "Signup failed";
+    }
+
+}
+?>
